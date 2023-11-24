f4d4ce28697aa2001d7cb37e2196247c427c45cf
Ovi22102611 <junaedrahman8@gmail.com>
2023-11-24 13:51:42 +0000

    Add project files.

---
+++ ConvertPoundsToTaka/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConvertPoundsToTaka.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public decimal AmountInPounds { get; set; }
+
+        public decimal? ConvertedAmount { get; set; }
+
+        private decimal PoundToTakaRate = 115.50m; // Example rate, adjust to the current rate
+
+        public void OnPost()
+        {
+            ConvertedAmount = AmountInPounds * PoundToTakaRate;
+        }
+    }
+}
